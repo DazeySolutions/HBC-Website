bf739d5e46bce432d7fb20d82a98b8b853526acd
Mark Dazey <markdazey@gmail.com>
2015-04-03 20:05:52 +0000

    try lg and primary

--- public_html/mysite/code/BootstrapLoginForm.php
+++ public_html/mysite/code/BootstrapLoginForm.php
@@ -8,12 +8,12 @@ class BootstrapLoginForm extends MemberLoginForm {
 		$emailField = new EmailField("Email", $label, null, null, $this);
 		$this->Fields()->replaceField("Email", $emailField);
 		$this->Fields()->bootstrapify();
-		$actionSubmit = new BootstrapFormAction('dologin', "Log in");
-		$actionSubmit->setStyle('primary');
-		$actionSubmit->setSize('lg');
+		$actionSubmit = new FormAction('dologin', "Log in");
 		$this->Actions()->replaceField('dologin', $actionSubmit);
 		$this->Actions()->bootstrapify();
 		$this->setTemplate("BootstrapForm");
+		$actionSubmit->setStyle('primary');
+		$actionSubmit->setSize('lg');
 		$this->invokeWithExtensions('updateBoostrapMemberLoginForm', $this);
 	}
 }
