7380e3f99e6f4e22a3265237256b5e2d117bbd81
Mark Dazey <markdazey@gmail.com>
2015-04-03 19:00:40 +0000

    change to custom login form

---
+++ public_html/mysite/code/BootstrapLoginForm.php
@@ -0,0 +1,15 @@
+<?php
+class BootstrapLoginForm extends MemberLoginForm {
+	public function __construct($controller = null, $name = null, $fields = null, $actions = null, $checkCurrentUser = true) {
+		if(!$controller) $controller = Controller::curr();
+		if(!$name) $name = "LoginForm";
+		parent::__construct($controller, $name, $fields, $actions, $checkCurrentUser);
+		$label=singleton('Member')->fieldLabel(Member::config()->unique_identifier_field);
+		$emailField = new EmailField("Email", $label, null, null, $this);
+		$this->Fields()->replaceField("Email", $emailField);
+		$this->Fields()->bootstrapify();
+		$this->Actions()->bootstrapify();
+		$this->setTemplate("BootstrapForm");
+		$this->invokeWithExtensions('updateBoostrapMemberLoginForm', $this);
+	}
+}
