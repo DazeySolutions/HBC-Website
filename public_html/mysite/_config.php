06763b6ecfee6f36ab7b471b3ab1b2d62532f0e4
Mark Dazey <markdazey@gmail.com>
2015-04-06 18:34:40 +0000

    update config

--- public_html/mysite/_config.php
+++ public_html/mysite/_config.php
@@ -9,3 +9,5 @@ $database = '';
 require_once('conf/ConfigureFromEnv.php');
 // Set the site locale
 i18n::set_locale('en_US');
+
+HtmlEditorConfig::get('cms')->setOption('content_css','themes/cusom/css/main.css');
