2db6ef36756d4de5db0d031e70767aaf0ffca9eb
root <root@server1.hbc-ky.com>
2015-04-06 16:36:58 -0400

    fix bld

--- themes/custom/grunt/concat.js
+++ themes/custom/grunt/concat.js
@@ -6,7 +6,7 @@ module.exports = {
       src: ['bower_components/jquery/dist/jquery.js',  'bower_components/angular/angular.js',  'bower_components/angular-ui-router/release/angular-ui-router.js',  'bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/angular-animate/angular-animate.js', 'bower_components/angularjs-toaster/toaster.js'],
       dest: 'dist/js/dependencies.min.js',
     },
-    dist: {
+    css: {
       src: ['bower_components/angularjs-toaster/toaster.cs', 'dist/css/main.css' ],
       dest: 'dist/css/main.css',
     },
