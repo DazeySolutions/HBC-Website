64473eabc04299fc38878936fc6e8343caa414a8
Mark Dazey <markdazey@gmail.com>
2015-04-06 19:51:46 +0000

    change to php config

--- public_html/mysite/_config.php
+++ public_html/mysite/_config.php
@@ -12,3 +12,4 @@ i18n::set_locale('en_US');
 
 HtmlEditorConfig::get('cms')->setOption('content_css','themes/custom/css/main.css');
 HtmlEditorConfig::get('cms')->setOption('body_class','section-row odd');
+Object::add_extension('HtmlEditorField_Toolbar', 'CustomHTMLEditorField');
