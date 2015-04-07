8adfa9100112a728f38369d49dffaa97b91ba513
Mark Dazey <markdazey@gmail.com>
2015-04-07 14:31:35 +0000

    extend member

---
+++ public_html/mysite/code/extensions/MyMember.php
@@ -0,0 +1,25 @@
+<?php
+
+class MyMember extends DataExtension {
+  /**
+
+   * Modify the field set to be displayed in the CMS detail pop-up
+   */
+    public function updateCMSFields(FieldList $currentFields) {
+        $ftpField = new TextField('ftpUser', 'FTP User Name');
+        $eventField = new TextField('eventsUser', 'Events User Name');
+		$fields->addFieldToTab("Root.OtherAccounts",$ftpField);
+		$fields->addFieldToTab("Root.OtherAccounts",$eventField);
+    }
+
+    // define additional properties
+    private static $db = array(
+        'ftpUser'=>'Text',
+        'eventsUser'=>'Text'
+        ); 
+    private static $has_one = array(); 
+    private static $has_many = array(); 
+    private static $many_many = array(); 
+    private static $belongs_many_many = array(); 
+
+}
