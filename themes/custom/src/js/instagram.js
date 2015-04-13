2de6c4e28242412fc8df3c297c4bd7b679b3a644
Mark Dazey <markdazey@gmail.com>
2015-04-13 15:47:59 +0000

    width changes

--- themes/custom/src/js/instagram.js
+++ themes/custom/src/js/instagram.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
   get:'user',
   userId: 1678475616,
   accessToken: '1678475616.467ede5.3507c96e1879422ba2bbef4eddc38025',
-  limit: 9,
+  limit: 12,
   template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
 });
 feed.run();
