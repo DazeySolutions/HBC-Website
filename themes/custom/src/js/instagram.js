a67bdfbf5d2c5475fb5341b91776f7dae6d62c76
Mark Dazey <markdazey@gmail.com>
2015-04-13 16:54:10 +0000

    restyle instagram

--- themes/custom/src/js/instagram.js
+++ themes/custom/src/js/instagram.js
@@ -4,7 +4,7 @@ var feed = new Instafeed({
   accessToken: '1678475616.467ede5.3507c96e1879422ba2bbef4eddc38025',
   limit: 12,
   resolution: 'standard_resolution',
-  template: '<div class="col-xs-12 col-sm-6 col-md-4"><a href="{{link}}" target="_blank"><div style="width:100%; height: 100%; position: relative;"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></div></a></div>'
+  template: '<div class="col-xs-12 col-sm-6 col-md-4"><a href="{{link}}" target="_blank"><div style="width:100%; height: 100%; position: relative;"><img src="{{image}}" /><div class="likes"><span class="heartsTable"><i class="heartsCell">&hearts; {{likes}}</i></span></div></div></a></div>'
 });
 feed.run();
 
