c3d83b7742763c0889134b74b87ac91cc057e949
Mark Dazey <markdazey@gmail.com>
2015-04-16 12:49:18 +0000

    setup content page controller

--- themes/custom/src/js/custom.js
+++ themes/custom/src/js/custom.js
@@ -96,30 +96,6 @@ hbcWebApp.controller('HomePageController', ['$scope', '$http', '$stateParams', '
             });
         });
         $scope.imagePath = "/"+location+"/ajaxImages";
-        // $http.get("/"+location+"/ajaxImages?width="+$window.innerWidth).success(function(data){
-        //     $scope.images = data;
-        //     $scope.prevImageNum = data.length-1;
-        //     $scope.currImage = $scope.images[0];
-            
-        //     angular.element(".imageSlider").css('background-image','url("/'+$scope.currImage.Filename+'")');
-        //     lodash.forEach($scope.images, function(image, index){
-        //         angular.element(".imageSlider .dots span").append("<i class='dot"+index+" fa fa-fw fa-circle-o'></i>");
-        //     });
-        //     angular.element(".imageSlider .dots span i.dot0").removeClass('fa-circle-o');
-        //     angular.element(".imageSlider .dots span i.dot0").addClass('fa-circle');
-        //     if($scope.images.length > 1){
-        //         $timeout(function(){
-        //             $scope.currImage = $scope.images[$scope.nextImageNum];
-        //             angular.element(".imageSlider").css('background-image','url("/'+$scope.currImage.Filename+'")');
-        //             $scope.incrementImageNums();
-        //             angular.element(".imageSlider .dots span i.fa-circle").addClass('fa-circle-o');
-        //             angular.element(".imageSlider .dots span i.fa-circle").removeClass('fa-circle');
-        //             angular.element(".imageSlider .dots span i.dot"+$scope.currImageNum).addClass('fa-circle');
-        //             angular.element(".imageSlider .dots span i.dot"+$scope.currImageNum).removeClass('fa-circle-o');
-        //         }, 7000);
-        //     }
-        // });
-        
     };
     var events;
     $scope.incrementImageNums = function(){
@@ -144,8 +120,61 @@ hbcWebApp.controller('HomePageController', ['$scope', '$http', '$stateParams', '
 
 hbcWebApp.controller('ContentPageController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
     $scope.init =  function init(){
-      
+        var location = '';
+        var maxHeight = $window.innerWidth/(2.39);
+        angular.element(".imageSlider").css('height',maxHeight+"px");
+        angular.element(".imageSlider").css('background-color','#222');
+        angular.element(".imageSlider").css('background-size','cover');
+        location = !angular.isUndefinedOrNullOrEmpty($stateParams.page) ? $stateParams.page : location;
+        if(location !== ''){
+            $http.get("/"+location+"/ajaxContent").success(function(data){
+                $scope.content = data;
+                if($scope.content.length % 2 === 1){
+                    angular.element(".connection").removeClass("odd");
+                    angular.element(".connection").addClass("even");
+                    angular.element(".footer .section-row").removeClass("even");
+                    angular.element(".footer .section-row").addClass("odd");
+                }
+                $http.get("http://events.hbc-ky.com/a").success(function(data){
+                    if(angular.isUndefinedOrNullOrEmpty(data)){
+                        events = -1;
+                    }else{
+                        events = data;
+                    }
+                    if(!angular.isUndefinedOrNull($scope.content)){
+                        if(events === -1){
+                            angular.element(".event-section").html("<h4 class='text-center'>More events coming soon!</h4>");
+                        }else{
+                            angular.element(".event-section").html(events);
+                            if(!$scope.$$phase) {
+                                $scope.$apply();
+                            }
+                        }
+                    }
+                });
+            });
+            $scope.imagePath = "/"+location+"/ajaxImages";
+        }
     };
+    var events;
+    $scope.incrementImageNums = function(){
+        $scope.nextImageNum++;
+        if($scope.nextImageNum >= $scope.images.length){
+            $scope.nextImageNum = 0;
+        }
+        $scope.curImageNum++;
+        $scope.prevImageNum++;
+        if($scope.prevImageNum >= $scope.images.length){
+            $scope.prevImageNum = $scope.images.length-1;
+        }
+    };
+    $scope.currImage = undefined;
+    $scope.currImageNum = 0;
+    $scope.nextImageNum = 1;
+    $scope.prevImageNum = -1;
+    $scope.content = undefined;
+    $scope.images = {};
+    $scope.init();
 }]);
 
 hbcWebApp.controller('FormPageController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
