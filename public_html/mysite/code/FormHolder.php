4ff205aec74b5c7a38d15cc6f89e3554ed25a9f3
Mark Dazey <markdazey@gmail.com>
2015-04-17 12:22:54 +0000

    add icons

--- public_html/mysite/code/FormHolder.php
+++ public_html/mysite/code/FormHolder.php
@@ -1,8 +1,9 @@
 <?php
 class FormHolder extends Page{
+    private static $icon = "mysite/images/icon/folder.png";
     private static $allowed_children = array(
-			"FormPage"	
-			);
+	    "FormPage"	
+	);
 }
 
 class FormHolderController extends Page_Controller{
