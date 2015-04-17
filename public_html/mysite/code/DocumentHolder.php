0f758995d8b4a38131c055ebb094bff61bbbf6d2
Mark Dazey <markdazey@gmail.com>
2015-04-17 12:45:50 +0000

    none

--- public_html/mysite/code/DocumentHolder.php
+++ public_html/mysite/code/DocumentHolder.php
@@ -50,6 +50,6 @@ class DocumentHolder_Controller extends Page_Controller{
 	);
 
     public function ajax() {
-		return $this->renderWith('Angular/DocumentHolder');
+		return $this->renderWith('DocumentHolder_A');
 	}
 }
