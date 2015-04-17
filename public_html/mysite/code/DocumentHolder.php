92f412815cfe55211379c42933d1f378932ca46e
Mark Dazey <markdazey@gmail.com>
2015-04-17 15:04:45 +0000

    changes

--- public_html/mysite/code/DocumentHolder.php
+++ public_html/mysite/code/DocumentHolder.php
@@ -47,14 +47,11 @@ class DocumentHolder extends Page {
 	    foreach($this->DocumentPages() as $doc) {
 	        $date = new Date();
 	        $date->setValue($doc->DocumentDate);
-	        echo $date;
+	        echo $date->Year();
 	        $year = $date->Year;
 	        $month = $date->Month;
 	        $day = $date->Day;
 	        $found = false;
-	        echo $year;
-	        echo $month;
-	        echo $day;
 	        foreach($retarr as $curArr){
 	            if($curArr['Year'] == $year){
 	                foreach($currArr['Months'] as $monthArr){
