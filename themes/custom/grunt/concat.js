1ce7b573d41a8ceed09beae6ffde808729b6d346
Mark Dazey <markdazey@gmail.com>
2015-04-17 15:06:14 +0000

    changes

--- themes/custom/grunt/concat.js
+++ themes/custom/grunt/concat.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     dist: {
         src: ['bower_components/jquery/dist/jquery.js',  
+              'bower_components/bootstrap/dist/js/bootstrap.js', 
               'bower_components/angular/angular.js', 
               'lib/instafeed.js', 
               'lib/ngImageSlide.js', 
@@ -11,7 +12,6 @@ module.exports = {
               'bower_components/pdfjs-dist/build/pdf.js',
               'bower_components/angular-pdf/dist/angular-pdf-viewer.js',
               'bower_components/angular-ui-router/release/angular-ui-router.js',  
-              'bower_components/bootstrap/dist/js/bootstrap.js', 
               'bower_components/angular-animate/angular-animate.js', 
               'bower_components/angularjs-toaster/toaster.js',
               'bower_components/ng-lodash/build/ng-lodash.js'],
