0aa5c2666a6aa852f6bf5c1146300713d82de285
Mark Dazey <markdazey@gmail.com>
2015-04-29 17:08:11 +0000

    ii

--- public_html/mysite/_config.php
+++ public_html/mysite/_config.php
@@ -12,5 +12,5 @@ i18n::set_locale('en_US');
 
 HtmlEditorConfig::get('cms')->setOption('content_css','themes/custom/css/main.css');
 HtmlEditorConfig::get('cms')->setOption('body_class','section-row odd');
-HtmlEditorConfig::get('cms')->setOption('extended_valid_elements', '*[*]');
+HtmlEditorConfig::get('cms')->setOption('extended_valid_elements', '+*[*]');
 Object::add_extension('HtmlEditorField_Toolbar', 'CustomHTMLEditorField');
