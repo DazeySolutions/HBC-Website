54513d571b4d295dcd94098fd8e066544024680b
Mark Dazey <markdazey@gmail.com>
2015-05-04 13:48:54 +0000

    remove ngMap from parameters

--- themes/custom/src/js/custom.js
+++ themes/custom/src/js/custom.js
@@ -67,7 +67,7 @@ hbcWebApp.config(['$locationProvider', function($locationProvider){
 * CONTROLLERS FILE
 * controllers.js
 */
-hbcWebApp.controller('SiteController', ['$scope', 'toaster', '$window', '$http', '$stateParams', '$state','loadGoogleMapAPI','ngMap', function ($scope, toaster, $window, $http, $stateParams, $state, loadGoogleMapAPI, ngMap){
+hbcWebApp.controller('SiteController', ['$scope', 'toaster', '$window', '$http', '$stateParams', '$state','loadGoogleMapAPI', function ($scope, toaster, $window, $http, $stateParams, $state, loadGoogleMapAPI){
     
     var marker, map;
     $scope.$on('mapInitialized', function(evt, evtMap) {
