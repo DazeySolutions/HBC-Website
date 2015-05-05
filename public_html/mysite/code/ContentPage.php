deee3bab214cb53d0fc3a88a141ab0f91bf0cc0c
Mark Dazey <markdazey@gmail.com>
2015-05-05 20:07:12 +0000

    sortable

--- public_html/mysite/code/ContentPage.php
+++ public_html/mysite/code/ContentPage.php
@@ -53,7 +53,7 @@ class ContentPage extends SiteTree implements PermissionProvider {
 		$gridFieldConfig2->addComponent(new GridFieldDeleteAction());
 		$gridFieldConfig2->addComponent($pagination = new GridFieldPaginator(10));
 		$gridFieldConfig2->addComponent(new GridFieldDetailForm());
-		$gridFieldConfig2->addComponent(new GridFieldOrderableRows('SortOrder'));
+		$gridFieldConfig2->addComponent(new GridFieldSortableRows('SortOrder'));
 		
 		
 		$gridFieldConfig = new GridFieldConfig();
@@ -64,7 +64,7 @@ class ContentPage extends SiteTree implements PermissionProvider {
 		$gridFieldConfig->addComponent(new GridFieldDeleteAction());
 		$gridFieldConfig->addComponent($pagination = new GridFieldPaginator(10));
 		$gridFieldConfig->addComponent(new GridFieldDetailForm());
-		$gridFieldConfig->addComponent(new GridFieldOrderableRows('SortOrder'));
+		$gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
 		
 		$gridFieldSlideShowImages = new GridField("SlideShowImages", "Background Slides", $this->SlideShowImages()->sort("SortOrder"), $gridFieldConfig2);
 		$gridFieldContentSections = new GridField("ContentSections", "Page Content Sections", $this->ContentSections()->sort("SortOrder"), $gridFieldConfig);
