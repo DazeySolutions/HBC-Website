a56d854a9d2c20472a3153c03e9b4851a356c8ea
Mark Dazey <markdazey@gmail.com>
2015-05-12 14:51:28 +0000

    add gallery page root

---
+++ public_html/mysite/code/GalleryHolder.php
@@ -0,0 +1,12 @@
+<?php
+class GalleryHolder extends Page{
+    private static $icon = "mysite/images/icon/galleries.png";
+    private static $allowed_children = array(
+	    "GalleryPage"	
+	);
+	
+}
+
+class FormHolderController extends Page_Controller{
+    
+}
