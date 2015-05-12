69bb41f8ff63ec932a072475c7f95a6ccb614bb7
Mark Dazey <markdazey@gmail.com>
2015-05-12 14:32:17 +0000

    contnet page icon

--- public_html/mysite/code/ContentPage.php
+++ public_html/mysite/code/ContentPage.php
@@ -1,6 +1,6 @@
 <?php
 class ContentPage extends SiteTree implements PermissionProvider {
-
+    private static $icon = "mysite/images/icon/page.png";
 	private static $db = array(
 	    "ImagePath"=>"Varchar",
 	    "OverrideImagePath"=>"Boolean"
