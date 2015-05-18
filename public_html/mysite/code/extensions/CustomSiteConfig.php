9b83c00a65446633d21bd553f8b3b9161601348e
Mark Dazey <markdazey@gmail.com>
2015-05-18 14:27:48 +0000

    try 3

--- public_html/mysite/code/extensions/CustomSiteConfig.php
+++ public_html/mysite/code/extensions/CustomSiteConfig.php
@@ -24,7 +24,7 @@ class CustomSiteConfig extends DataExtension {
 	);
 	
 	public function onBeforeWrite(){
-	    $txt =      "@odd:                  #".$this->owner->BackgroundOneColor.";\n";
+	    $txt =      "@odd:                  #".$this->owner->BackgroundOneColor::HEX().";\n";
 	    $txt .=     "@even:                 #".$this->owner->BackgroundTwoColor.";\n";
 	    $txt .=     "@header:               #".$this->owner->HeaderColor.";\n";
 	    $txt .=     "@odd-text:             #".$this->owner->TextOneColor.";\n";
