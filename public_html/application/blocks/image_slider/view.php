70ed4bda721ea96c7802f2e7d3c81bfc1945e127
mrd030485 <markdazey@gmail.com>
2015-06-05 09:45:53 -0400

    update

---
+++ public_html/application/blocks/image_slider/view.php
@@ -0,0 +1,40 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+$navigationTypeText = ($navigationType == 0) ? 'arrows' : 'pages';
+$c = Page::getCurrentPage();
+if ($c->isEditMode()) { ?>
+    <div class="ccm-edit-mode-disabled-item" style="width: <?php echo $width; ?>; height: <?php echo $height; ?>">
+        <div style="padding: 40px 0px 40px 0px"><?php echo t('Image Slider disabled in edit mode.')?></div>
+    </div>
+<?php  } else { ?>
+<script>
+    var appDependencies = ['ngImageSlider'];
+    var imageApp = angular.module("imageApp", appDependencies);
+    angular.isUndefinedOrNull = function undefinedOrNull(value){
+        return angular.isUndefined(value) || value === null;
+    };
+    angular.isUndefinedOrNullOrEmpty = function undefinedOrNull(value){
+        return angular.isUndefined(value) || value === null || value === "";
+    };
+imageApp.controller('SlideController', ['$scope', function($scope){
+        $scope.images = [];
+        <?php if(count($rows) > 0) { ?>
+            <?php foreach($rows as $row) { ?>
+                <?php
+                $f = File::getByID($row['fID'])
+                ?>
+            $scope.images.push({
+                'link': '<?php echo $row['linkURL'] ?>',
+                'title': '<?php echo $row['title'] ?>',
+                'image': '<?php echo $f->getRelativePath() ?>',
+                'description': '<?php echo $row['description'] ?>'
+            });
+            <?php } ?>
+        <?php } else { ?>
+        <?php } ?>
+    }]);
+</script>
+<div ng-app="imageApp" ng-controller="SlideController">
+    <div ng-image-slider data="images">
+    </div>
+</div>
+<?php } ?>
