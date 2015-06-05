0957cf9d491f4eb309f50e852b01e8cf3059d413
Mark Dazey <markdazey@gmail.com>
2015-06-05 14:53:09 +0000

    remove grid from main content

--- themes/hbc/full.php
+++ themes/hbc/full.php
@@ -16,7 +16,6 @@
         ?>
         <?php
             $a = new Area('Main');
-            $a->enableGridContainer();
             $a->display($c);
         ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
