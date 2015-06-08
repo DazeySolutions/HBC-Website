e97153398f6479cc2f729f7a898c0e3977334630
Mark Dazey <markdazey@gmail.com>
2015-06-08 14:31:19 +0000

    fix section-row placement

--- public_html/application/blocks/feature_columns/view.php
+++ public_html/application/blocks/feature_columns/view.php
@@ -1,7 +1,7 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
-<div class="row">
+<div class="row section-row">
 	<?php if($numberOfColumns == 0) { ?>
-	<div class="ccm-one-column section-row">
+	<div class="ccm-one-column">
 	    <div class="col-xs-12 col-md-10 col-md-offset-1">
 	        <div class="col-xs-12 col-md-6 col-md-offset-3">
 	            <hr class="hidden-xs col-sm-3">
@@ -14,7 +14,7 @@
 	    </div>
 	</div>
 	<?php }else if($numberOfColumns == 1){ ?>
-	<div class="ccm-two-column section-row">
+	<div class="ccm-two-column">
 	    <div class="col-xs-12 col-md-10 col-md-offset-1">
 	        <div class="col-xs-12 col-md-6">
 	            <hr class="hidden-xs col-sm-3">
@@ -35,7 +35,7 @@
 	    </div>
 	</div>
 	<?php }else { ?>
-	<div class="ccm-three-column section-row">
+	<div class="ccm-three-column">
 	    <div class="col-xs-12 col-md-10 col-md-offset-1">
 	        <div class="col-xs-12 col-md-4">
 	            <hr class="hidden-xs col-sm-3">
