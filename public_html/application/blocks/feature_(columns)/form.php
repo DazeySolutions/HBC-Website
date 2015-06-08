455a4085f05ed28ab1dc4698478bc9eaf61de1e7
Mark Dazey <markdazey@gmail.com>
2015-06-08 14:10:27 +0000

    feature columns

---
+++ public_html/application/blocks/feature_(columns)/form.php
@@ -0,0 +1,58 @@
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<fieldset>
+	<legend><?php echo t("Number of Columns")?></legend>
+	<div class="form-group num-cols">
+		<?php echo $form->label('numberOfColumns', t('Columns'));?>
+        <?php echo $form->select('numberOfColumns', array("1","2","3"), $numberOfColumns); ?>
+        <script>
+        	$(".num-cols select").change(function(){
+        		var numCols = $(".num-cols select option:selected").val();
+        		if(numCols == 1){
+        			$(".col-two").hide
+        			$(".col-three").hide();
+        		}else if(numCols == 2){
+        			$(".col-two").show();
+        			$(".col-three").hide();
+        		}else{
+        			$(".col-two").show();
+        			$(".col-three").show();
+        		}
+        	});
+        </script>
+	</div>
+</fieldset>
+<fieldset>
+	<legend><?php echo t('Column One')?></legend>
+    <div class="form-group">
+        <?php echo $form->label('columnOneTitle', t('Title'));?>
+        <?php echo $form->text('columnOneTitle', $columnOneTitle); ?>
+    </div>
+    <div class="form-group">
+        <?php echo $form->label('columnOneContnet', t('Content'));?>
+        <?php echo $form->textarea('columnOneContnet', $columnOneContnet); ?>
+    </div>
+</fieldset>
+
+<fieldset class="col-two">
+	<legend><?php echo t('Column Two')?></legend>
+    <div class="form-group">
+        <?php echo $form->label('columnTwoTitle', t('Title'));?>
+        <?php echo $form->textarea('columnTwoTitle', $columnTwoTitle); ?>
+    </div>
+    <div class="form-group">
+        <?php echo $form->label('columnTwoContnet', t('Content'));?>
+        <?php echo $form->textarea('columnTwoContnet', $columnTwoContnet); ?>
+    </div>
+</fieldset>
+
+<fieldset class="col-three">
+	<legend><?php echo t('Column Three')?></legend>
+    <div class="form-group">
+        <?php echo $form->label('columnThreeTitle', t('Title'));?>
+        <?php echo $form->textarea('columnThreeTitle', $columnThreeTitle); ?>
+    </div>
+    <div class="form-group">
+        <?php echo $form->label('columnThreeTitle', t('Content'));?>
+        <?php echo $form->textarea('columnThreeTitle', $columnThreeTitle); ?>
+    </div>
+</fieldset>
---
+++ public_html/application/blocks/feature_(columns)/view.php
@@ -0,0 +1,65 @@
+<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php if($numberOfColumns == 1) { ?>
+<div class="ccm-one-column section-row">
+    <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <div class="col-xs-12 col-md-6 col-md-offset-3">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnOneTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+        </div>
+    	<div class="col-xs-12">
+    		<?php echo $columnOneContent ?>
+    	</div>
+    </div>
+</div>
+<?php }else if($numberOfColumns == 2){ ?>
+<div class="ccm-two-column row section-row">
+    <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <div class="col-xs-12 col-md-6">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnOneTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+		    <div class="col-xs-12">
+		    	<?php echo $columnOneContent ?>
+		    </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnTwoTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+		    <div class="col-xs-12">
+		    	<?php echo $columnTwoContent ?>
+		    </div>
+        </div>
+    </div>
+</div>
+<?php }else { ?>
+<div class="ccm-three-column row section-row">
+    <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <div class="col-xs-12 col-md-4">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnOneTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+		    <div class="col-xs-12">
+		    	<?php echo $columnOneContent ?>
+		    </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnTwoTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+		    <div class="col-xs-12">
+		    	<?php echo $columnTwoContent ?>
+		    </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <hr class="hidden-xs col-sm-3">
+            <h2 class="col-xs-12 col-sm-6"><?php echo $columnThreeTitle ?></h2>
+            <hr class="hidden-xs col-sm-3">
+		    <div class="col-xs-12">
+		    	<?php echo $columnThreeContent ?>
+		    </div>
+        </div>
+    </div>
+</div>
+<?php } ?>
