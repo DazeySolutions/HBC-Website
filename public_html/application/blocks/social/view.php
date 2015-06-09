9b5847ae8a7dbed7f7a73943dce0ca20a2b52779
Mark Dazey <markdazey@gmail.com>
2015-06-09 14:53:41 +0000

    test

--- public_html/application/blocks/social/view.php
+++ public_html/application/blocks/social/view.php
@@ -4,9 +4,6 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 <div id="ccm-block-social-links<?php echo $bID?>" class="ccm-block-social-links">
     <ul class="list-inline">
-    <?php foreach($socialData as $data) {
-        ?>
-        <li><a href="<?php echo $data['url'] ?>"><i class="fa fa-fw fa-<?php echo $data['icon'] ?>"></i></a></li>
-    <?php } ?>
+    
     </ul>
 </div>
