df9e2bd007ca3173295107e785618a421923c40b
Mark Dazey <markdazey@gmail.com>
2015-06-09 14:54:50 +0000

    test

--- public_html/application/blocks/social/view.php
+++ public_html/application/blocks/social/view.php
@@ -1,9 +1,3 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 ?>
-
-<div id="ccm-block-social-links<?php echo $bID?>" class="ccm-block-social-links">
-    <ul class="list-inline">
-    
-    </ul>
-</div>
