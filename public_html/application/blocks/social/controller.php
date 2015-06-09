d5bef1ad05f08fad87fc17d528c49745b4ea4684
Mark Dazey <markdazey@gmail.com>
2015-06-09 15:17:47 +0000

    test

--- public_html/application/blocks/social/controller.php
+++ public_html/application/blocks/social/controller.php
@@ -70,7 +70,7 @@ class Controller extends BlockController
 		$showIDs = $data['showID'];
 		$feedIDs = $data['feedID'];
 		$access = $data['access'];
-		shell_exec("echo ".var_dump($data)." > /home/hbc/web/site/out.txt");
+		shell_exec("echo test > /home/hbc/web/site/out.txt");
 		$links = Link::getList();
     	$socialData = array();
     	foreach($links as $link){
