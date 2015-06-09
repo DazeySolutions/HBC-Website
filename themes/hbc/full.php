d7b4b5448d5f4980b9e9b6b7528e51f5df33ee49
Mark Dazey <markdazey@gmail.com>
2015-06-09 15:37:02 +0000

    test

--- themes/hbc/full.php
+++ themes/hbc/full.php
@@ -2,42 +2,40 @@
 <html lang="en">
     <?php $this->inc('elements/head.php'); ?> 
     <body>
-    <script>
-        $(document).ready(function(){
-            $("body .section-row:odd").addClass("even");
-            $("body .section-row:even").addClass("odd");
-        });
-        <?php if($c->isEditMode()){ ?>
-        	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-			var observer = new MutationObserver(function(mutations, observer) {
-		        $("body .section-row:odd").addClass("even");
+	    <script>
+	        $(document).ready(function(){
+	            $("body .section-row:odd").addClass("even");
 	            $("body .section-row:even").addClass("odd");
-			});
-			observer.observe(document, {
-			  subtree: true,
-			  attributes: true
-			});
-        <?php } ?>
-    </script>
-    <div class="<?php echo $c->getPageWrapperClass()?>">
-        <?php $this->inc('elements/navigation.php'); ?> 
-        <?php
-            $a = new Area('Slide');
-            $a->display($c);
-        ?>
-        <?php
-            $a = new Area('Main');
-            $a->display($c);
-        ?>
-      </div>
-      <div class="section-row">
-        <?php
-          $a = new GlobalArea('Connection');
-          $a->enableGridContainer(); 
-          $a->display();
-        ?>
-      </div>
-      <?php $this->inc('elements/footer.php'); ?> 
+	        });
+	        <?php if($c->isEditMode()){ ?>
+	        	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	
+				var observer = new MutationObserver(function(mutations, observer) {
+			        $("body .section-row:odd").addClass("even");
+		            $("body .section-row:even").addClass("odd");
+				});
+				observer.observe(document, {
+				  subtree: true,
+				  attributes: true
+				});
+	        <?php } ?>
+	    </script>
+	    <div class="<?php echo $c->getPageWrapperClass()?>">
+	        <?php $this->inc('elements/navigation.php'); ?> 
+	        <?php
+	            $a = new Area('Slide');
+	            $a->display($c);
+	        ?>
+	        <?php
+	            $a = new Area('Main');
+	            $a->display($c);
+	        ?>
+	        <?php
+	          $a = new GlobalArea('Connection');
+	          $a->enableGridContainer(); 
+	          $a->display();
+	        ?>
+	      	<?php $this->inc('elements/footer.php'); ?> 
+	    </div>
     </body>
 </html>
