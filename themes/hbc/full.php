8c2e0a120e3c4101c475b115cdab7d0a8f71274e
Mark Dazey <markdazey@gmail.com>
2015-06-10 13:42:18 +0000

    change full

--- themes/hbc/full.php
+++ themes/hbc/full.php
@@ -27,61 +27,11 @@
 	            $a->display($c);
 	        ?>
 	        <div class="container-fluid">
-	        	<div class="row">
-	        		<div class="col-xs-12 col-md-10 col-md-offset-1">
-	        			<div class="section-row">
-					        <?php
-					            $a = new Area('Content Section One');
-					            $a->setAreaGridMaximumColumns(12);
-					            $a->display($c);
-					        ?>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-	        		<div class="col-xs-12 col-md-10 col-md-offset-1">
-	        			<div class="section-row">
-					        <?php
-					            $a = new Area('Content Section Two');
-					            $a->setAreaGridMaximumColumns(12);
-					            $a->display($c);
-					        ?>
-						</div>
-					</div>
-			    </div>
-				<div class="row">
-	        		<div class="col-xs-12 col-md-10 col-md-offset-1">
-	        			<div class="section-row">
-					        <?php
-					            $a = new Area('Content Section Three');
-					            $a->setAreaGridMaximumColumns(12);
-					            $a->display($c);
-					        ?>
-						</div>
-					</div>
-			    </div>
-				<div class="row">
-	        		<div class="col-xs-12 col-md-10 col-md-offset-1">
-	        			<div class="section-row">
-					        <?php
-					            $a = new Area('Content Section Four');
-					            $a->setAreaGridMaximumColumns(12);
-					            $a->display($c);
-					        ?>
-						</div>
-					</div>
-			    </div>
-				<div class="row">
-	        		<div class="col-xs-12 col-md-10 col-md-offset-1">
-	        			<div class="section-row">
-					        <?php
-					            $a = new Area('Content Section Five');
-					            $a->setAreaGridMaximumColumns(12);
-					            $a->display($c);
-					        ?>
-						</div>
-					</div>
-			    </div>
+		        <?php
+		            $a = new Area('Main');
+		            $a->setAreaGridMaximumColumns(12);
+		            $a->display($c);
+		        ?>
 		    </div>
 	        <?php
 	          $a = new GlobalArea('Connection');
