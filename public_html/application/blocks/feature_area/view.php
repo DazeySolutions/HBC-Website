ebf3fd122416a6e44e09b53dba0bb361905d8d83
Mark Dazey <markdazey@gmail.com>
2015-06-10 13:47:56 +0000

    try new method for adding bootstrap

--- public_html/application/blocks/feature_area/view.php
+++ public_html/application/blocks/feature_area/view.php
@@ -3,7 +3,7 @@
 	<?php $c = Page::getCurrentPage(); ?>
 	<?php
         $a = new Area($areaName);
-        if($bootstrap3){
+        if($bootstrap3 == 1){
         	$a->setAreaGridMaximumColumns(12);
         }
         $a->display($c);
