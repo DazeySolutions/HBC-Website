06133d8cbc5f5bd70a78a47c2f69e614c1f28f5d
Mark Dazey <markdazey@gmail.com>
2015-06-10 14:07:12 +0000

    try something form a forum

--- public_html/application/blocks/feature_area/view.php
+++ public_html/application/blocks/feature_area/view.php
@@ -1,9 +1,18 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php $c = Page::getCurrentPage(); ?>
+<?php if($c->isEditMode()) { ?>
+	</div>
+<?php } ?>
+
 <div class="row section-row" style="z-index: 2000;">
-	<?php $c = Page::getCurrentPage(); ?>
+
 	<?php
         $a = new Area($areaName);
     	$a->setAreaGridMaximumColumns(12);
         $a->display($c);
     ?>
 </div>
+
+<?php if($c->isEditMode()) { ?>
+	<div>
+<?php } ?>
