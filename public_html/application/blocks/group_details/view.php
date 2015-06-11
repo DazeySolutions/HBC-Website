fff2be7fad35a360a101e8a300e494588dc047e5
Mark Dazey <markdazey@gmail.com>
2015-06-11 18:35:16 +0000

    details

--- public_html/application/blocks/group_details/view.php
+++ public_html/application/blocks/group_details/view.php
@@ -6,10 +6,14 @@
 		<hr class="hidden-xs col-sm-3">
 		<?php foreach($userData as $user){ ?>
 		<div class="col-xs-12 col-sm-4">
-			<?php echo $user->getAttribute('real_name'); ?>
 			<?php $im = $user->getAttribute('profile_image'); ?>
-			<img src="<?php echo $im->getRelativePath(); ?>" >
-			<?php echo $user->getAttribute('details'); ?>
+			<img src="<?php echo $im->getRelativePath(); ?>" style="width: 100%;">
+			<div class="text-center col-xs-12">
+				<h4><?php echo $user->getAttribute('real_name'); ?></h4>
+				<h5><?php echo $user->getAttribute('title'); ?></h5>
+				<p data-toggle="collapse" data-target="#<?php echo $user->getUserID()?>Details">Details</p>
+				<p id="<?php echo $user->getUserID()?>Details"><?php echo $user->getAttribute('details'); ?></p>
+			</div>
 		</div>
 		<?php } ?>
 	</div>
