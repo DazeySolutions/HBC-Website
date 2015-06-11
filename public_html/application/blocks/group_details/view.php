72ba932baa0c6f6af8df7c7ef10356684397856a
Mark Dazey <markdazey@gmail.com>
2015-06-11 18:41:40 +0000

    reformat

--- public_html/application/blocks/group_details/view.php
+++ public_html/application/blocks/group_details/view.php
@@ -7,13 +7,13 @@
 		<?php foreach($userData as $user){ ?>
 		<div class="col-xs-12 col-sm-4">
 			<?php $im = $user->getAttribute('profile_image'); ?>
-			<a data-toggle="collapse" href="#<?php echo $user->getUserID()?>Details">
-				<img data-toggle="tooltip" data-placement="top" title="<h3>Click for more info</h3>" src="<?php echo $im->getRelativePath(); ?>" style="width: 100%;">
-			</a>
 			<div class="text-center col-xs-12">
 				<h4><?php echo $user->getAttribute('real_name'); ?></h4>
 				<h5><?php echo $user->getAttribute('title'); ?></h5>
-				<div id="<?php echo $user->getUserID()?>Details"><?php echo $user->getAttribute('details'); ?></div>
+			</div>
+			<img src="<?php echo $im->getRelativePath(); ?>" style="width: 100%;">
+			<div class="text-center col-xs-12">
+				<?php echo $user->getAttribute('details'); ?>
 			</div>
 		</div>
 		<?php } ?>
