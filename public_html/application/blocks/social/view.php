2bbbe296446e8b71e2ca6361afbde44d4d03afca
Mark Dazey <markdazey@gmail.com>
2015-06-11 13:59:16 +0000

    changes

--- public_html/application/blocks/social/view.php
+++ public_html/application/blocks/social/view.php
@@ -6,7 +6,7 @@ if ($c->isEditMode()) { ?>
         <div style="padding: 40px 0px 40px 0px"><?php echo t('Social (Link/Feed) disabled in edit mode.')?></div>
     </div>
 <?php  } else { ?>
-	<div class="section row connection">
+	<div class="row section connection">
 		<?php if(count($socialData != 0)){ ?>
 			<div class="col-xs-12 col-md-10 col-md-offset-1">
 			<?php $feedCount = 0;
