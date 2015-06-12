1ad1ebfafd8c054c4af23141a35d2443da4be77c
Mark Dazey <markdazey@gmail.com>
2015-06-12 14:09:00 +0000

    oo

--- public_html/application/blocks/filesets/view.php
+++ public_html/application/blocks/filesets/view.php
@@ -5,25 +5,27 @@ $.getScript("<?php echo $view->getThemePath() ?>/assets/js/bootstrap-treeview.mi
 	var tree = [
 	<?php foreach($docs as $key=>$doc){?>
 		{
-			text: <?php echo $key ?>,
+			text: "<?php echo $key ?>",
 			<?php if(is_array($doc)) {?>
 			nodes: [
 				<?php foreach($doc as $sub_key=>$doc_2){?>	
-					text: <?php echo $sub_key ?>,
+					text: "<?php echo $sub_key ?>",
 					<?php if(is_array($doc_2)) {?>
 					nodes: [
 						<?php foreach($doc as $sub_sub_key=>$doc_3){?>	
-							text: <?php echo $sub_sub_key ?>,
-							href: <?php echo $doc_3?>
+						{
+							text: "<?php echo $sub_sub_key ?>",
+							href: "<?php echo $doc_3 ?>""
+						},
 						<?php } ?>		
 					]
 					<?php } else {?>
-					href: <?php echo $doc_2?>
+					href: "<?php echo $doc_2?>"
 					<?php } ?>
 				<?php } ?>	
 			]
 			<?php } else {?>
-			href: <?php echo $doc?>
+			href: "<?php echo $doc?>"
 			<?php } ?>	
 		},
 	<?php } ?>
